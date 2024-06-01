e01dd6efbc2b660010da08d33859d822c4f49e75
Cing2PO <169150691+Cing2PO@users.noreply.github.com>
2024-06-01 12:46:42 +0700

    Update pohon.py
    add user input

--- pohon.py
+++ pohon.py
@@ -16,6 +16,15 @@ class treenode:
             p = p.parent
         return level
 
+    def find_node(self, parent):
+        if self.data == parent:
+            return self
+        for child in self.child:
+            found_node = child.find_node(parent)
+            if found_node:
+                return found_node
+        return None
+
     def print_tree(self):
         spaces = " " * self.get_level() * 3
         print(spaces + self.data)
@@ -23,30 +32,35 @@ class treenode:
             for child in self.child:
                 child.print_tree()
 
-def plant_tree():
-    root = treenode("car")
+def make_child(val,parent,root):
+    parent = root.find_node(parent)
+    if parent:
+        parent.add_child(treenode(val))
+    else:
+        print("Parent node not found")
 
-    tuner = treenode("tuner")
-    tuner.add_child(treenode("Nissan"))
-    tuner.add_child(treenode("Toyota"))
-    tuner.add_child(treenode("Honda"))
 
-    muscle = treenode("muscle")
-    muscle.add_child(treenode("Ford"))
-    muscle.add_child(treenode("Chevrolet"))
-    muscle.add_child(treenode("Dodge"))
-
-    exotic = treenode("exotic")
-    exotic.add_child(treenode("Ferrari"))
-    exotic.add_child(treenode("Lamborghini"))
-
-    root.add_child(tuner)
-    root.add_child(muscle)
-    root.add_child(exotic)
-
-    return root
+def main():
+    while True:
+        print("pilih program")
+        print("1.tanam pohon")
+        print("2.buat anak")
+        print("3.print tree")
+        print("4.break")
+        x = int(input("masukkan pilihan: "))
+        if x==1:
+            val = input("masukan nilai yang ingin dimasukkan: ")
+            root = treenode(val)
+        elif x==2:
+            val = input("masukkan nilai yang ingin dimasukkan: ")
+            parent = input("masukkan cabang: ")
+            make_child(val,parent,root)
+        elif x==3:
+            root.print_tree()
+        elif x=4:
+            break
+        else:
+            print("pilihan tidak tersedia")
 
 if __name__ == '__main__':
-    root = plant_tree()
-    #print(root.get_level())
-    root.print_tree()
+    main()
